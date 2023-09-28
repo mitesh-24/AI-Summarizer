06eaea4f655d7168a063b3e3947669fcd5d37692
mitesh-24 <miteshhk05@gmail.com>
2023-09-28 22:33:22 +0530

    initialised ptoject AI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function myApp() {
+    return(
+        <div>
+            <h1>
+                Hello there
+            </h1>
+        </div>
+    )
+}
